b760b3f4e5ab779c8a69dca1ea68f962c907da7b
wcs1010270451 <296952503@163.com>
2019-02-18 00:27:38 +0800

    个人中心修改

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify'=>true]);
 
 //Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UsersController');
