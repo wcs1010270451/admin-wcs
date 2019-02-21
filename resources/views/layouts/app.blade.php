aa8def50e1c4a5ed08b79500ebe05e23f4a6c587
wcs1010270451 <296952503@163.com>
2019-02-22 01:04:49 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>@yield('title', '页面') - 嘉宾CRM</title>
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   @yield('styles')
