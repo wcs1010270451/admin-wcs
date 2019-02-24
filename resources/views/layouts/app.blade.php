0b79747fac63bbc521ad22e12efb18ba9864cc1e
wcs1010270451 <296952503@163.com>
2019-02-24 20:36:30 +0800

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,9 @@
     </div>
     @include('layouts._footer')
   </div>
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
   @yield('scripts')
