03042032ea8935036cde4b3d2dbe4783fdd01bd9
wcs1010270451 <296952503@163.com>
2019-02-24 15:38:18 +0800

    回复列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,24 +16,26 @@
       </div>
     </div>
   </div>
-  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-    <div class="card ">
-      <div class="card-body">
-          <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
-      </div>
-    </div>
-    <hr>
-
-    {{-- 用户发布的内容 --}}
-    <div class="card">
+  <div class="card ">
       <div class="card-body">
         <ul class="nav nav-tabs">
-          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+          <li class="nav-item">
+            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('users.show', $user->id) }}">
+              Ta 的话题
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
+              Ta 的回复
+            </a>
+          </li>
         </ul>
-        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @if (if_query('tab', 'replies'))
+          @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+        @else
+          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @endif
       </div>
-    </div>
 
   </div>
 </div>
